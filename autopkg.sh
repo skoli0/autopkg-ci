164d7b0d6232ed2b10c557f2073b6bc4196c4b34
Sandeep Koli <30632063+skoli0@users.noreply.github.com>
2021-04-06 14:26:50 +0530

    Add script to process local recipes

---
+++ autopkg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+AUTOPKGRECIPES=($(find $(pwd) -name '*.munki.*' -exec basename {} \;))
+AUTOPKG=$(which autopkg)
+MUNKIMAKECATALOGS="/usr/local/munki/makecatalogs"
+MUNKIICONIMPORTER="/usr/local/munki/iconimporter"
+MANIFESTUTIL="/usr/local/munki/manifestutil"
+
+aLen=${#AUTOPKGRECIPES[@]}
+echo "$aLen" "overrides to create"
+
+for (( j=0; j<aLen; j++));
+do
+    echo "Adding ${AUTOPKGRECIPES[$j]} override"
+    ${AUTOPKG} make-override -f "${AUTOPKGRECIPES[$j]}.munki.recipe"
+    echo "Updating trust info for ${AUTOPKGRECIPES[$j]}"
+    ${AUTOPKG} update-trust-info "${AUTOPKGRECIPES[$j]}.munki.recipe"
+    echo "Added ${AUTOPKGRECIPES[$j]} override"
+    echo "Running ${AUTOPKGRECIPES[$j]} recipe"
+    ${AUTOPKG} run "${AUTOPKGRECIPES[$j]}.munki.recipe"
+done
+
+${MANIFESTUTIL} new-manifest site_default
+echo "site_default created"
+${MANIFESTUTIL} add-catalog testing --manifest site_default
+echo "Testing Catalog added to site_default"
+
+listofpkgs=($(${MANIFESTUTIL} list-catalog-items testing))
+echo "List of Packages for adding to repo:" ${listofpkgs[*]}
+
+no_of_packages=${#listofpkgs[@]}
+echo "$no_of_packages" " packages to add to manifest"
+
+for (( i=0; i<no_of_packages; i++));
+do
+    echo "Adding ${listofpkgs[$i]} to site_default"
+    ${MANIFESTUTIL} add-pkg ${listofpkgs[$i]} --manifest site_default
+    echo "Added ${listofpkgs[$i]} to site_default"
+done
+
+find /Users/Shared/munki_repo
